a51b84ebad2ebcaa389e57c9a7265a85b523a2df
isaacacurio660011-sudo <isaacacurio660011@gmail.com>
2026-01-14 18:17:45 -0500

    Clean up ESPELINGOS.cpp by removing comments
    Removed comments and unnecessary code from ESPELINGOS.cpp.

--- ESPELINGOS/ESPELINGOS.cpp
+++ ESPELINGOS/ESPELINGOS.cpp
@@ -1,10 +1,3 @@
-// ESPELINGOS.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
-#include <iostream>
-include namespace std
-int main()
-{
 //Menu Leccion
 //5 opciones: ingreso de datos, mostrar datos, ejericio1, ejercicio2, salir ademas una interfaz agradable
 
@@ -89,24 +82,3 @@ void menuUsuario() {
     } while(op != 5);
 }
 
-//Ingreso de datos
-//Datos del usuario nombre, edad, carrera, año, universidad
-//Mostrar datos
-// mostrar los datos ingresados
-
-//Ejercicio español a ingles
-//Que el programa envie una palabra en español y el usuario mande la respuesta y que se verifique si es correcta
-
-//Ejercicio ingles a español
-//Que el programa envie una palabra en ingles y el usuario mande la respuesta y que se verifique si es correcta
-}
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
